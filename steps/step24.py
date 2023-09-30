52b05640ea52929702030272c979fc409a25f164
statelesshz <hzji210@gmail.com>
2023-09-30 16:00:55 +0800

    step24: some misc(complex expression)

---
+++ steps/step24.py
@@ -0,0 +1,16 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+def sphere(x, y):
+    return x ** 2 + y **  2
+
+
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = sphere(x, y)
+z.backward()
+print(x.grad, y.grad)
