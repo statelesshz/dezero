74ceff6fd5847a9ba024453788ec1b27ac4c69d8
statelesshz <hzji210@gmail.com>
2023-10-01 13:51:37 +0800

    implement transpose & reshape

--- dezero/core.py
+++ dezero/core.py
@@ -3,6 +3,8 @@ import contextlib
 
 import numpy as np
 
+import dezero
+
 class Variable:
     __array_priority__ = 200
 
@@ -48,6 +50,18 @@ class Variable:
 
     def cleargrad(self):
         self.grad = None
+
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
+    
+    def transpose(self):
+        return dezero.functions.transpose(self)
+    
+    @property
+    def T(self):
+        return dezero.functions.transpose(self)
 
     def backward(self, retain_grad=False, create_graph=False):
         if self.grad is None:
--- dezero/functions.py
+++ dezero/functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from dezero.core import Function
+from dezero.core import Function, as_variable
 
 
 class Sin(Function):
@@ -28,7 +28,26 @@ class Tanh(Function):
     def backward(self, gy):
         y = self.outputs[0]()
         return gy * (1 - y * y)
+    
 
+class Reshape(Function):
+    def __init__(self, shape):
+        self.shape = shape
+    
+    def forward(self, x):
+        self.x_shape = x.shape
+        return x.reshape(self.shape)
+    
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
+    
+
+class Transpose(Function):
+    def forward(self, x):
+        return np.transpose(x)
+    
+    def backward(self, gy):
+        return transpose(gy)
 
 
 def sin(x):
@@ -43,4 +62,11 @@ def tanh(x):
     return Tanh()(x)
 
 
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
 
+
+def transpose(x):
+    return Transpose()(x)
