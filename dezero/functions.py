f303563875330eea894c39e2b1ce7ec2bb386d74
statelesshz <hzji210@gmail.com>
2023-10-04 11:52:41 +0800

    implement linear&sigmoid

--- dezero/functions.py
+++ dezero/functions.py
@@ -162,3 +162,39 @@ def matmul(x, W):
 
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
+
+
+class Linear(Function):
+    """
+    Copy from https://github.com/oreilly-japan/deep-learning-from-scratch-3/blob/06419d7fb2e7ea19aa3719efc27795edbdc41a1f/dezero/functions.py#L273C1-L289C29
+    """
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+class Sigmoid(Function):
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
